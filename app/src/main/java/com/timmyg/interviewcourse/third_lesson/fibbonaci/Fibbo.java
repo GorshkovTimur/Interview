eb6bf81823ea80311d108b429dd36dd934cbe3fc
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-19 18:57:41 +0300

    done 2nd task

---
+++ app/src/main/java/com/timmyg/interviewcourse/third_lesson/fibbonaci/Fibbo.java
@@ -0,0 +1,8 @@
+package com.timmyg.interviewcourse.third_lesson.fibbonaci;
+
+public class Fibbo {
+    public static void main(String[] args) {
+        Fibbonaci fibbo = new Fibbonaci(50);
+        fibbo.getFibboNumbers();
+    }
+}
